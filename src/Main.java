41b4f86a747c06c7523693e544a48dc5b5575226
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-21 14:25:34 -0700

    bstory

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.*;
+import java.util.Scanner;
+class Program {
+    public static void main(String args[]) {
+        try {
+            FileInputStream DataFiles = new FileInputStream("C:\\Users\\Carlos Sarabia\\Desktop\\Year Up United Courses\\Learn to Code Academy LTCA\\DataFiles\\Bedtime Stories\\goldilocks.txt");
+            Scanner scanner = new Scanner(DataFiles);
+            String input;
+            while (scanner.hasNextLine()) {
+                input = scanner.nextLine();
+                System.out.println(input);
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
